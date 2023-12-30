4d56aabf2314e2d427a44fa29cc218ceb7a4e9e9
Lartaxx <didou.com@gmail.com>
2023-12-31 00:04:13 +0100

    Create the base api structure

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-// This is the default root (https://api.intersoft.fr/).
+// This is the default root (https://api.reputation-system.fr/).
 // Don't put any other route in this file.
 
 module.exports = {
